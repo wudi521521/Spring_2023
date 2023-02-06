640bf4cdf340b2769bf12714234fcf79b182916d
Wu <qianqianwu@QianqiandeMacBook-Pro-2.local>
2023-02-06 18:19:58 +0800

    提交

---
+++ src/main/java/com/wudi/springannotation/SpringAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.wudi.springannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnnotationApplication.class, args);
+    }
+
+}
